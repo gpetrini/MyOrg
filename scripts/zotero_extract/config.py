90aedd6c04e937350186f7571225a8e57a0bcce0
gpetrini <gpetrinidasilveira@gmail.com>
2026-05-12 12:02:32 -0300

    [Pre Unicamp] Add zotero parser

--- scripts/zotero_extract/config.py
+++ scripts/zotero_extract/config.py
@@ -16,5 +16,5 @@ FALLBACK_SECTION = "** Interesting findings and not categorized stuff"
 ANNOTATIONS_SECTION = "* Annotations (zotero)"
 UNCATEGORIZED_SECTION = "* Uncategorized annotations"
 ZOTERO_BASE = "http://localhost:23119/api/users/0"
-ZOTERO_API_KEY = ""  # set your local API key here
+ZOTERO_API_KEY = "qVe5qfhvanFWIJbQl0PL0SUy"  # set your local API key here
 ORG_SEARCH_DIR = "~/Org"
